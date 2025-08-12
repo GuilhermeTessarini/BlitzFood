8c6a2d7a3d16421e4a23319a4734be6138bae32c
Guilherme Tessarini <guilherme.tessarini@hotmail.com>
2025-08-12 15:24:14 -0300

    feat: implement custom auth UI and Appwrite integration
    -Introduced CustomButton and CustomInput components for reusable form controls.
    -Refactored sign-in and sign-up screens to use these components and added form state management and validation.
    -Added Appwrite config file for backend integration.
    -Updated auth layout for improved UI with images.
    -Added type definitions for core entities.

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,71 @@
-import { router } from "expo-router";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link, router } from "expo-router";
 import React from "react";
-import { Button, Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 
 const SignUp = () => {
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const [form, setForm] = React.useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const submit = async () => {
+    if (!form.name || !form.email || !form.password)
+      return
+      Alert.alert("Error", "Please enter valid name, email and password");
+
+    setIsSubmitting(true);
+    try {
+      // Call Appwrite sign-up API here
+      Alert.alert("Success", "User signed up successfully!");
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>Sign Up</Text>
-      <Button title="Sign up" onPress={() => router.push("/sign-in")} />
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput
+        placeholder="Enter your full name"
+        value={form.name}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, name: text }))}
+        label="Full Name"
+      />
+
+      <CustomInput
+        placeholder="Enter your email"
+        value={form.email}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
+        label="Email"
+        keyboardType="email-address"
+      />
+
+      <CustomInput
+        placeholder="Enter your password"
+        value={form.password}
+        onChangeText={(text) =>
+          setForm((prev) => ({ ...prev, password: text }))
+        }
+        label="Password"
+        secureTextEntry
+      />
+
+      <CustomButton title="Sign Up" isLoading={isSubmitting} onPress={submit} />
+
+      <View className="flex justify-center nt-5 flex-row gap-2">
+        <Text className="base-regular text-grey-100">
+          Already have an account?
+        </Text>
+        <Link href="/sign-in" className="base-bold text-primary">
+          Sign In
+        </Link>
+      </View>
     </View>
   );
 };
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.blitz.blitzfood",
+    databaseId: "689a2a3f0015a29a4a6a",
+    userCollectionId: "689a2a68003dc0c47050"
+}
